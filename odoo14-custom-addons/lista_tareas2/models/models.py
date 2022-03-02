d4b1f079c962c0b7504822d83d07bd0536272c4c
Alejandro <alejandrogotojob.com>
2022-03-02 21:19:34 +0100

    actualizacion 2

--- odoo14-custom-addons/lista_tareas2/models/models.py
+++ odoo14-custom-addons/lista_tareas2/models/models.py
@@ -7,12 +7,22 @@ class lista_tareas2(models.Model):
     _name = 'lista_tareas2.lista_tareas2'
     _description = 'lista_tareas2.lista_tareas2'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    avatar = fields.Image('Imagen tarea', max_width=50,max_heigth=50)
+    fecha = fields.Date()
+    tarea = fields.Char()
+    prioridad = fields.Integer()
+    urgente = fields.Boolean(compute="_value_urgente", store=True)
+    realizada = fields.Boolean()
+    descripcion = fields.Text()
 
-    @api.depends('value')
-    def _value_pc(self):
+    #Este computo depende de la variable prioridad
+    @api.depends('prioridad')
+    #Funcion para calcular el valor de urgente
+    def _value_urgente(self):
+        #Para cada registro
         for record in self:
-            record.value2 = float(record.value) / 100
+            #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
+            if record.prioridad>6:
+                record.urgente = True
+            else:
+                record.urgente = False
